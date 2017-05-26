62a962fea33061c6e07db8e460ac8825a2ac454c
Marcin Rapacz <marcin.rapacz@elfinite.com>
2017-05-26 12:58:58 +0200

    Integration with redis part 3.

--- back-end/suggester/corpus.py
+++ back-end/suggester/corpus.py
@@ -11,12 +11,16 @@ class Corpus(object):
         self.models = models
         self.redis_instance = StrictRedis(host='localhost', port=6379, db=0)
 
-    def make_predictions(self, beginning, number_of_sentence_predictions=5, max_number_of_words_predictions=4):
-
+    def make_predictions(self, beginning,
+                         number_of_sentence_predictions=5,
+                         max_number_of_words_predictions=4,
+                         number_of_combinations=4):
         input = [beginning, float(0)]
 
         sentences_predictions = \
-            self.make_sentence_predictions(input, number_of_sentence_predictions, max_number_of_words_predictions)
+            self.make_sentence_predictions(input, number_of_combinations, max_number_of_words_predictions)
+
+        print("No of sentences: ", len(sentences_predictions))
 
         sorted_sentences_predictions = sorted(sentences_predictions, key=itemgetter(1), reverse=True)
 
@@ -32,20 +36,18 @@ class Corpus(object):
 
         return [" ".join(item) for item in normalized_predictions]
 
-    def make_sentence_predictions(self, input, number_of_sentence_predictions, max_number_of_words_predictions):
-
+    def make_sentence_predictions(self, input, number_of_combinations, max_number_of_words_predictions):
         initial_len = len(input[0])
 
-        sentences = self.run_predictions_cycle(input, number_of_sentence_predictions, max_number_of_words_predictions, initial_len)
-
-        all = []
-
-        print("First: ", sentences)
+        sentences = self.run_predictions_cycle(input,
+                                               number_of_combinations,
+                                               max_number_of_words_predictions,
+                                               initial_len)
 
         for _ in range(max_number_of_words_predictions-1):
             enhanced_sentences = []
             for sentence in sentences:
-                result = self.run_predictions_cycle(sentence, number_of_sentence_predictions, max_number_of_words_predictions, initial_len)
+                result = self.run_predictions_cycle(sentence, number_of_combinations, max_number_of_words_predictions, initial_len)
                 enhanced_sentences = enhanced_sentences + result
 
             sentences = enhanced_sentences
@@ -54,51 +56,27 @@ class Corpus(object):
 
     def move_window(self, input, window_width):
         return input[-window_width:]#return last "window_width" elements
-        # return [input[0][-window_width:], input[1]] #return last "window_width" elements
 
-    def run_predictions_cycle(self, input, number_of_sentence_predictions, max_number_of_words_predictions, initial_len):
+    def run_predictions_cycle(self, input, number_of_combinations, max_number_of_words_predictions, initial_len):
         sentence_predictions = []
 
         beginning = self.move_window(input[0], window_width=initial_len)
 
         key_name = self.generate_key(self.models, beginning)
 
-        possible_words = self.predict_words(key_name)
+        possible_words = self.predict_words(key_name, number_of_combinations)
         possible_words_len = len(possible_words)
 
-        for i in range(possible_words_len) if possible_words_len < number_of_sentence_predictions else range(number_of_sentence_predictions):
+        for i in range(possible_words_len) if possible_words_len < number_of_combinations else range(number_of_combinations):
             sentence = [] + input[0]
             sentence.append(possible_words[i][0].decode("utf-8"))
 
             score = input[1] + possible_words[i][1]
 
             prediction = [sentence, score]
-
             sentence_predictions.append(prediction)
 
-        # for __ in range(max_number_of_words_predictions):
-
         return sentence_predictions
-
-    def make_single_prediction(self, beginning, max_number_of_words_in_prediction):
-        # for model in self.models:
-        prediction = [] + beginning
-        key_name = self.generate_key(self.models, beginning)
-
-        for _ in range(max_number_of_words_in_prediction):
-            possible_words = self.predict_words(key_name)
-
-            prediction.append(possible_words[0][0].decode("utf-8"))
-
-            key_name = self.generate_key(self.models, prediction[1:])
-
-            possible_words = self.predict_words(key_name)
-
-            prediction.append(possible_words[0][0].decode("utf-8"))
-
-        prediction = " ".join(prediction)
-
-        return prediction
 
     def predict_words(self, key_name, number_of_words=5):
         words = self.redis_instance.zrange(name=key_name,
--- back-end/suggester/suggester.py
+++ back-end/suggester/suggester.py
@@ -4,10 +4,11 @@ from suggester import Text, Corpus
 
 
 def suggest(query, numberOfWords=2, **kwargs):
-    if(len(query['words']) < numberOfWords):
+    if (len(query['words']) < numberOfWords):
         return []
     else:
         return predict(query)
+
 
 def predict(query, **kwargs):
     jargon = query['jargon']
@@ -16,27 +17,31 @@ def predict(query, **kwargs):
     corpus = Corpus(models=get_models(jargon))
 
     predictions = corpus.make_predictions(beginning=words,
-                            number_of_sentence_predictions=4,
-                            max_number_of_words_predictions=4)
+                                          number_of_sentence_predictions=4,
+                                          max_number_of_words_predictions=4,
+                                          number_of_combinations=7)
 
-    return list(set(predictions)) # remove duplicates
+    return list(set(predictions))  # remove duplicates
+
 
 def get_default_model():
     return DEFAULT_MODEL['key']
+
 
 def get_models(jargon):
     selected_model = get_default_model()
 
     try:
-        if(jargon == 'default'):
+        if (jargon == 'default'):
             return selected_model['key']
         else:
             for model in MODELS:
-                if(MODELS[model]['name'] == jargon):
+                if (MODELS[model]['name'] == jargon):
                     selected_model = MODELS[model]['key']
             return selected_model
     except ValueError:
         return selected_model
+
 
 MODELS = {
     'wiki_partial': {
