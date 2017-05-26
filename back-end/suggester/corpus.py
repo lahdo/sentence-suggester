0b3c4ace196feee507096c64eb16c19588d965fe
Marcin Rapacz <marcin.rapacz@elfinite.com>
2017-05-26 12:26:13 +0200

    Integration with redis part 2.

--- back-end/suggester/corpus.py
+++ back-end/suggester/corpus.py
@@ -1,5 +1,5 @@
 from .redis import StrictRedis
-
+from operator import itemgetter
 
 class Corpus(object):
     """
@@ -11,14 +11,74 @@ class Corpus(object):
         self.models = models
         self.redis_instance = StrictRedis(host='localhost', port=6379, db=0)
 
-    def make_predictions(self, beginning, number_of_predictions=5, max_number_of_words_in_prediction=4):
-        predictions = []
+    def make_predictions(self, beginning, number_of_sentence_predictions=5, max_number_of_words_predictions=4):
 
-        for _ in range(number_of_predictions):
-            prediction = self.make_single_prediction(beginning, max_number_of_words_in_prediction)
-            predictions.append(prediction)
+        input = [beginning, float(0)]
 
-        return predictions
+        sentences_predictions = \
+            self.make_sentence_predictions(input, number_of_sentence_predictions, max_number_of_words_predictions)
+
+        sorted_sentences_predictions = sorted(sentences_predictions, key=itemgetter(1), reverse=True)
+
+        if len(sorted_sentences_predictions) > number_of_sentence_predictions:
+            predictions = sorted_sentences_predictions[:number_of_sentence_predictions]
+        else:
+            predictions = [] + sorted_sentences_predictions
+
+        return self.normalize_prediction(predictions)
+
+    def normalize_prediction(self, predictions):
+        normalized_predictions = [item[0] for item in predictions]
+
+        return [" ".join(item) for item in normalized_predictions]
+
+    def make_sentence_predictions(self, input, number_of_sentence_predictions, max_number_of_words_predictions):
+
+        initial_len = len(input[0])
+
+        sentences = self.run_predictions_cycle(input, number_of_sentence_predictions, max_number_of_words_predictions, initial_len)
+
+        all = []
+
+        print("First: ", sentences)
+
+        for _ in range(max_number_of_words_predictions-1):
+            enhanced_sentences = []
+            for sentence in sentences:
+                result = self.run_predictions_cycle(sentence, number_of_sentence_predictions, max_number_of_words_predictions, initial_len)
+                enhanced_sentences = enhanced_sentences + result
+
+            sentences = enhanced_sentences
+
+        return sentences
+
+    def move_window(self, input, window_width):
+        return input[-window_width:]#return last "window_width" elements
+        # return [input[0][-window_width:], input[1]] #return last "window_width" elements
+
+    def run_predictions_cycle(self, input, number_of_sentence_predictions, max_number_of_words_predictions, initial_len):
+        sentence_predictions = []
+
+        beginning = self.move_window(input[0], window_width=initial_len)
+
+        key_name = self.generate_key(self.models, beginning)
+
+        possible_words = self.predict_words(key_name)
+        possible_words_len = len(possible_words)
+
+        for i in range(possible_words_len) if possible_words_len < number_of_sentence_predictions else range(number_of_sentence_predictions):
+            sentence = [] + input[0]
+            sentence.append(possible_words[i][0].decode("utf-8"))
+
+            score = input[1] + possible_words[i][1]
+
+            prediction = [sentence, score]
+
+            sentence_predictions.append(prediction)
+
+        # for __ in range(max_number_of_words_predictions):
+
+        return sentence_predictions
 
     def make_single_prediction(self, beginning, max_number_of_words_in_prediction):
         # for model in self.models:
@@ -44,7 +104,7 @@ class Corpus(object):
         words = self.redis_instance.zrange(name=key_name,
                                            start=0,
                                            end=number_of_words,
-                                           desc=False,
+                                           desc=True,
                                            withscores=True)
 
         return words
--- back-end/suggester/suggester.py
+++ back-end/suggester/suggester.py
@@ -16,8 +16,8 @@ def predict(query, **kwargs):
     corpus = Corpus(models=get_models(jargon))
 
     predictions = corpus.make_predictions(beginning=words,
-                            number_of_predictions=4,
-                            max_number_of_words_in_prediction=1)
+                            number_of_sentence_predictions=4,
+                            max_number_of_words_predictions=4)
 
     return list(set(predictions)) # remove duplicates
 
@@ -37,68 +37,6 @@ def get_models(jargon):
             return selected_model
     except ValueError:
         return selected_model
-
-# def get_model(jargon):
-#     selected_model = get_default_model()
-#
-#     try:
-#         if(jargon == 'default'):
-#             return selected_model()
-#         else:
-#             for model in MODELS:
-#                 if(MODELS[model]['name'] == jargon):
-#                     selected_model = MODELS[model]['model']
-#             return selected_model()
-#     except ValueError:
-#         return selected_model()
-#
-# def get_books_models():
-#     models = []
-#
-#     for i in range(5):#20
-#         with open('./models/book_models/%s.json' % str(i+1)) as data_file:
-#             models.append(Text.from_json(json.load(data_file)))
-#
-#     return markovify.combine(models)
-#
-# def get_sherlock_model():
-#     with open('./models/sherlock_corpus.json') as data_file:
-#         model_json = json.load(data_file)
-#
-#     return Text.from_json(model_json)
-#
-# def get_wiki_partial_model():
-#     with open('./models/wiki_models/wikipedia_partial.json') as data_file:
-#         model_json = json.load(data_file)
-#
-#     return Text.from_json(model_json)
-#
-# def get_news_model():
-#     # with open('./models/news_models/news.json') as data_file:
-#     #     model_json = json.load(data_file)
-#     #
-#     # return Text.from_json(model_json)
-# #     return ''
-#
-#
-# MODELS = {
-#     'wiki_partial': {
-#         'name': 'Wikipedia',
-#         'model': get_wiki_partial_model,
-#     },
-#     'sherlock': {
-#         'name': 'Sherlock',
-#         'model': get_sherlock_model,
-#     },
-#     'news': {
-#         'name': 'News',
-#         'model': get_news_model,
-#     },
-#     'books': {
-#         'name': 'Books',
-#         'model': get_books_models,
-#     },
-# }
 
 MODELS = {
     'wiki_partial': {
