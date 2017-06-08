eca0b028152fe002eca5d5a089bc1966d5700c88
Marcin Rapacz <marcin.rapacz@elfinite.com>
2017-06-08 10:59:21 +0200

    Pasting text has removed formatting.

--- front-end/src/components/Page.js
+++ front-end/src/components/Page.js
@@ -6,9 +6,17 @@ export default class Page extends Component {
         super(props);
     }
 
+    onPaste(e) {
+        e.preventDefault();
+        var text = e.clipboardData.getData("text/plain");
+        document.execCommand("insertHTML", false, text);
+    }
+
     render() {
         return (
-            <div onKeyDown={this.props.onKeyDown.bind(this)}
+            <div ref="pageContent"
+                onPaste={this.onPaste.bind(this)}
+                onKeyDown={this.props.onKeyDown.bind(this)}
                  onClick={ this.props.onClick.bind(this) }
                  onInput={ this.props.onInput.bind(this) }
                  className="page"
